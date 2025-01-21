8dc9dbf3196c5f6f522549c8201036991318a926
Florin M <florinmaracine@yahoo.com>
2025-01-21 20:02:31 +0000

    Add remove item functionality to shopping cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('', views.view_cart, name='cart_view'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -20,3 +20,14 @@ def view_cart(request):
     """Displays the user's shopping cart."""
     cart = Cart.objects.filter(user=request.user).first()  # Retrieve the user's cart
     return render(request, 'cart/cart_view.html', {'cart': cart})
+
+
+def remove_from_cart(request, item_id):
+    """Remove an item from the cart."""
+    cart_item = get_object_or_404(CartItem, id=item_id)
+
+    # Ensure the item belongs to the logged-in user
+    if cart_item.cart.user == request.user:
+        cart_item.delete()
+
+    return redirect('cart_view')
