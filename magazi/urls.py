6cbb08ca891cc0a62ccfdabe27dbb7b9f19a6077
Florin M <florinmaracine@yahoo.com>
2025-01-28 22:30:50 +0000

    Add checkout functionality with Stripe integration and update cart template

--- magazi/urls.py
+++ magazi/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ]
