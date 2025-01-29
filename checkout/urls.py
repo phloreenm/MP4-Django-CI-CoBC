0d7f62cb3649ebecadc5be28470f879146bdea7b
Florin M <florinmaracine@yahoo.com>
2025-01-29 01:45:45 +0000

    Add order form and update checkout templates for improved payment processing

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'checkout'
 
 urlpatterns = [
-    path('', views.checkout, name='checkout'),  # Pagina de Checkout
-    path('create-checkout-session/', views.create_checkout_session, name='create_checkout_session'),
+    path('', views.checkout, name='checkout'),
+    path('create-checkout-session/', views.create_checkout_session, name='create_checkout_session'),  # Stripe session
     path('success/', views.success, name='success'),
     path('cancel/', views.cancel, name='cancel'),
+    path('process_payment/', views.process_payment, name='process_payment'),
 ]
