4c89336d6ec0631afc6952914df3c16b778f4b75
Florin M <florinmaracine@yahoo.com>
2025-02-04 23:59:45 +0000

    Add custom account adapter to add new client to Clients group by default and populate checkout form with profile data

--- orders/forms.py
+++ orders/forms.py
@@ -10,6 +10,18 @@ class OrderForm(forms.ModelForm):
             'town_or_city', 'postcode', 'country',
             'county', 'comments'
         ]
+        widgets = {
+            'full_name': forms.TextInput(attrs={'placeholder': 'Full Name', 'autocomplete': 'on'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email Address', 'autocomplete': 'on'}),
+            'phone_number': forms.TextInput(attrs={'placeholder': 'Phone Number', 'autocomplete': 'on'}),
+            'street_address1': forms.TextInput(attrs={'placeholder': 'Street Address 1', 'autocomplete': 'on'}),
+            'street_address2': forms.TextInput(attrs={'placeholder': 'Street Address 2', 'autocomplete': 'on'}),
+            'town_or_city': forms.TextInput(attrs={'placeholder': 'Town or City', 'autocomplete': 'on'}),
+            'postcode': forms.TextInput(attrs={'placeholder': 'Postal Code', 'autocomplete': 'on'}),
+            'country': forms.TextInput(attrs={'placeholder': 'Country', 'autocomplete': 'on'}),
+            'county': forms.TextInput(attrs={'placeholder': 'County', 'autocomplete': 'on'}),
+            'comments': forms.Textarea(attrs={'placeholder': 'Additional comments', 'autocomplete': 'on'}),
+        }
 
 class OrderLookupForm(forms.Form):
     order_number = forms.CharField(
