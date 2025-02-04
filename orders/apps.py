cf058aa5ea9b5667747770489fbf03e84ebc6ccd
Florin M <florinmaracine@yahoo.com>
2025-02-04 11:59:24 +0000

    Update orders app label and fix ForeignKey references for consistency

--- orders/apps.py
+++ orders/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
-    label = 'orders_app'  # Set a unique label different from "orders"
+    label = 'orders'
