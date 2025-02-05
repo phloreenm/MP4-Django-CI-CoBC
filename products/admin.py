52ce30e546fee51545d4e0c73e7ab985dc9c333f
Florin M <florinmaracine@yahoo.com>
2025-02-05 02:26:00 +0000

    Update ProductAdmin to display stock and digital file availability in the admin list view

--- products/admin.py
+++ products/admin.py
@@ -4,4 +4,8 @@ from .models import Product
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'created_at', 'updated_at')
+    list_display = ('name', 'price', 'stock', 'has_digital_file', 'created_at', 'updated_at')
+
+    def has_digital_file(self, obj):
+        return "Yes" if obj.digital_file else "No"
+    has_digital_file.short_description = "Digital File Available"
