be0d9cc46f86055b34ea26d02cecfbbeac542cd9
Florin M <florinmaracine@yahoo.com>
2025-08-01 01:32:14 +0300

    Add custom media serving for Heroku production

--- home/views.py
+++ home/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.conf import settings
+import os
+import mimetypes
 
 # Create your views here.
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def serve_media(request, path):
+    """Serve media files in production when DEBUG=False"""
+    if not settings.DEBUG:
+        # Build the full path to the file
+        file_path = os.path.join(settings.MEDIA_ROOT, path)
+        
+        # Check if file exists
+        if os.path.exists(file_path):
+            # Determine the content type
+            content_type, _ = mimetypes.guess_type(file_path)
+            
+            # Read and return the file
+            with open(file_path, 'rb') as f:
+                response = HttpResponse(f.read(), content_type=content_type)
+                return response
+        else:
+            raise Http404("Media file not found")
+    else:
+        raise Http404("This view is only for production")
--- magazi/urls.py
+++ magazi/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static # to serve media files in development
+from home.views import serve_media
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +31,10 @@ urlpatterns = [
 ]
 
 # Serve media files in both development and production
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Custom media serving for production (Heroku)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve_media, name='media'),
+    ]
